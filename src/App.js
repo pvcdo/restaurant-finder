4d7ab496c5c21e26affa58dfccfe6831620f2ddb
Paulo Oliveira <paulocivictor@gmail.com>
2021-09-08 10:42:18 -0300

    conteudo lateral aula 2 - styled-reset e config aside

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
+import { ThemeProvider } from 'styled-components'; // encapsula o documento principal de temas para usarmos em todas as renderizações da nossa aplicação.
 
 import theme from './theme';
 import Home from './pages/Home'; //quando não passamos qual arquivo deve ser encontrado dentro da pasta, o program porcura por padrão o que tenha o nome de index.
@@ -7,6 +8,7 @@ import Home from './pages/Home'; //quando não passamos qual arquivo deve ser en
 function App(){
   return(
     <ThemeProvider theme={theme}>
+      <Reset/>
       <Home/>
     </ThemeProvider>
   );
