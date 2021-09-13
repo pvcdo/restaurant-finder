e7703df78a9f4a4d9d30ae4cab68408ad74f825c
Paulo Oliveira <paulocivictor@gmail.com>
2021-09-13 12:07:36 -0300

    mapa-criado busca-ativada apikey-errada

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Card} from './ImageCard'; // estou exportando com o nome de Card a exportação default do index da pasta ImageCard
 export {default as RestaurantCard} from './RestautantCard';
 export {default as Modal} from './Modal';
+export {default as Map} from './Map';
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,12 +7,13 @@ import MaterialIcon from "@material/react-material-icon";
 import logo from '../../assets/logo.svg';
 import fotoRestaurante from '../../assets/restaurante-fake.png';
 
-import {Wrapper, Container, Logo, Search, CarouselTitle, Carousel, Map} from './styles';
-import {Card, RestaurantCard, Modal} from '../../components';
+import {Wrapper, Container, Logo, Search, CarouselTitle, Carousel, /*Map*/} from './styles';
+import {Card, RestaurantCard, Modal, Map} from '../../components';
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('')
     const [modalOpened, setModalOpened] = useState(true)
+    const [query, setQuery] = useState(null);
     
     const settings = {
         dots: false,
@@ -22,6 +23,13 @@ const Home = () => {
         slidesToScroll: 2,
         adaptiveHeight: true
     };
+
+    function handleKeyPress(e){
+        if(e.key === 'Enter'){
+            setQuery(inputValue)
+        }
+    }
+
     return(
         <Wrapper>
             <Container>
@@ -35,6 +43,7 @@ const Home = () => {
                         <Input 
                             value={inputValue}
                             onChange={(e) => setInputValue(e.target.value)}
+                            onKeyPress={handleKeyPress}
                         />
                     </TextField>
                     <CarouselTitle>Na sua área</CarouselTitle>
@@ -49,8 +58,8 @@ const Home = () => {
                 </Search>
                 <RestaurantCard/>                
             </Container>
-            <Map/>
-            <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)}>Para parar de abrir o modal automaticamente, trocar de true para false o modalOpened no index da Home --- Para fechar o modal, clicar fora ou apertar esc</Modal>
+            <Map query={query} />
+            {/*<Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)}>Para parar de abrir o modal automaticamente, trocar de true para false o modalOpened no index da Home --- Para fechar o modal, clicar fora ou apertar esc</Modal>*/}
         </Wrapper>
     )
 }
